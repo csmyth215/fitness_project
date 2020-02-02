24774ea1a54523a9103905127aa404c859965f1c
Chloe Smyth <chloe.smyth57@gmail.com>
2020-02-02 12:33:01 +0000

    Retrieve gpx for each activity

--- get_gpx.py
+++ get_gpx.py
@@ -13,32 +13,40 @@ cookies = {}
 for key, morsel in cookie.items():
     cookies[key] = morsel.value
 
+
+def get_activity_list():
+    url = 'https://connect.garmin.com/modern/proxy/activitylist-service/activities/search/activities?limit=500&start=0'
+    response = requests.get(url, cookies=cookies,)
+    if not response.ok:
+        raise Exception("Could not retrieve activity list: %s" % (response.status_code))
+    return response
+
+
 class Activity:
 
     def __init__(self, activity_id):
         self.activity_id = activity_id
 
-
     def get_activity_gpx(self):
         url = 'https://connect.garmin.com/modern/proxy/download-service/export/gpx/activity/' + str(self.activity_id)
-        # headers = {'referer': 'https://connect.garmin.com/modern/activity/' + str(self.activity_id), 'authority': 'connect.garmin.com'}
+        headers = {'referer': 'https://connect.garmin.com/modern/activity/' + str(self.activity_id), 'authority': 'connect.garmin.com'}
         response = requests.get(url, cookies=cookies, headers=headers)
         if not response.ok:
             raise Exception("Could not retrieve activity %s: %s" % (self.activity_id, response.status_code))
-
-    def get_activity_id_list():
-        url = 'https://connect.garmin.com/modern/proxy/activitylist-service/activities/search/activities?limit=1&start=0'
-        response = requests.get(url, cookies=cookies,)
-        if not response.ok:
-            raise Exception("Could not retrieve activity list: %s" % (response.status_code))
         return response
 
+activities_list = get_activity_list().json()
+activity_ids = []
+for activity in activities_list:
+    activity_ids.append(activity['activityId'])
 
-test_activity = Activity.get_activity_id_list()
-activities_list = test_activity.json()
-# activity_ids = []
-# for activity in activities_list:
-#     activity_ids.append(activity['activityId'])
-# return(activity_ids)
+count = 0
+for id in activity_ids:
+    activity = Activity(id)
+    print(f"Processing {count} of {len(activity_ids)}")
+    response = activity.get_activity_gpx()
+    with open(f"./gpx/{id}.gpx", "wb") as gpx_handle:
+        gpx_handle.write(response.content)
+    count += 1
 
-print(activities_list)
+
