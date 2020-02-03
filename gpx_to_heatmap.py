5f30c940c45dc56a9f6a86840edc6e089fe77bca
Chloe Smyth <chloe.smyth57@gmail.com>
2020-02-03 07:14:26 +0000

    Create heatmap with multiple activities

--- gpx_to_heatmap.py
+++ gpx_to_heatmap.py
@@ -1,22 +1,32 @@
 import xml.etree.ElementTree as ET
 import gmplot
+import os
+
+# set key with environment variables (dotenv)
+
 
 ns = {'default_ns': "http://www.topografix.com/GPX/1/1", 'xsi': "http://www.w3.org/2001/XMLSchema-instance", 
 'ns2': "http://www.garmin.com/xmlschemas/GpxExtensions/v3", 'ns3': "http://www.garmin.com/xmlschemas/TrackPointExtension/v1"}
 
-tree = ET.parse('xxx.gpx')
-root = tree.getroot()
-root_tag = root.tag
-root_attribute = root.attrib
+files = os.listdir("./gpx/")
 
-our_track = root.find('default_ns:trk', ns)
-if our_track is None:
-    raise Exception("Could not retrieve requested track")
-else:
-    track_points = []
-    for track_seg in our_track.findall('default_ns:trkseg', ns):
-        for trkpt in track_seg.findall('default_ns:trkpt', ns):
-            track_points.append(trkpt.attrib)
+track_points = []
+for filename in files:
+    try:
+        tree = ET.parse(f'./gpx/{filename}')
+        root = tree.getroot()
+        our_track = root.find('default_ns:trk', ns)
+        if our_track is None:
+            raise Exception("Could not retrieve requested track")
+        else:
+            for track_seg in our_track.findall('default_ns:trkseg', ns):
+                for trkpt in track_seg.findall('default_ns:trkpt', ns):
+                    track_points.append(trkpt.attrib)
+
+    except ET.ParseError as ex:
+        print(f"Encountered an error: {ex}")
+        continue
+        
 
 lats = []
 lons = []
